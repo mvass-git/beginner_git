f6fcef2f8f273d9c440b85cd2a1fa03674ca2462
Mykhailo Vasyliev <mvass.pe@gmail.com>
2025-03-26 19:21:09 +0200

    designed buttons

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ class LikeApp(App):
         self.mainBox = BoxLayout(orientation="vertical")
         self.lbl_title = Label(text='rate this picture', size_hint=[1, 0.1], font_size=32)
         self.img = Image(source="photo_2024-10-08_16-20-36.jpg")
-        btn_layout = BoxLayout(size_hint=[1, 0.2])
+        btn_layout = BoxLayout(size_hint=[1, 0.2], padding=50, spacing=50)
 
-        btn_like = Button(text="Like", font_size=24, size_hint=[0.45,0.7])
-        btn_dislike = Button(text="disike", font_size=24, size_hint=[0.45,0.7])
+        btn_like = Button(text="Like", font_size=24, size_hint=[0.45,0.7], padding=50, background_color=[0, 1, 0,1])
+        btn_dislike = Button(text="disike", font_size=24, size_hint=[0.45,0.7], padding=50, background_color=[1,0,0,1])
 
         self.mainBox.add_widget(self.lbl_title)
         self.mainBox.add_widget(self.img)
