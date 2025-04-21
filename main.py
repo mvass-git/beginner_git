7e35181a4881802db640be5d57d315e9d53c8d35
Mykhailo Vasyliev <mvass.pe@gmail.com>
2025-04-21 20:15:03 +0300

    done without buttons

--- main.py
+++ main.py
@@ -4,6 +4,43 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.image import Image
+from kivy.core.image import Image as CoreImage
+from kivy.clock import Clock
+
+import socket
+import json
+import threading
+import base64
+import os
+import io
+
+HOST, PORT = '127.0.0.1', 9999
+
+def send_request(action, data, callback):
+    def worker():
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((HOST, PORT))
+            request = {"action":action}
+            if data:
+                request.update(data)
+            s.sendall(json.dumps(request).encode())
+            response_data = b''
+            while True:
+                chunk = s.recv(4096)
+                if not chunk:
+                    break
+                response_data += chunk
+            response = json.loads(response_data.decode())
+        except Exception as e:
+            response = {"status":"error", "error":f"{e}"}
+        finally:
+            s.close()
+        Clock.schedule_once(lambda c: callback(response))
+    threading.Thread(target=worker, daemon=True).start()
+        
+
+
 
 class LikeApp(App):
     def build(self):
@@ -11,6 +48,9 @@ class LikeApp(App):
         self.lbl_title = Label(text='rate this picture', size_hint=[1, 0.1], font_size=32)
         self.img = Image(source="photo_2024-10-08_16-20-36.jpg")
         btn_layout = BoxLayout(size_hint=[1, 0.4], padding=50, spacing=50)
+
+        self.current_image_id = 0
+        self.current_rating = 0
 
         self.stars = []
 
@@ -28,6 +68,7 @@ class LikeApp(App):
         self.mainBox.add_widget(self.img)
 
         self.mainBox.add_widget(btn_layout)
+        self.request_image('get_next')
 
         return self.mainBox
 
@@ -40,5 +81,51 @@ class LikeApp(App):
             else:
                 self.stars[i].background_normal = "beginner_git/star0.png"
                 self.stars[i].background_down = "beginner_git/star0.png"
+        self.current_rating = index + 1
+    
+    def request_image(self, action):
+        send_request(action, None, self.on_image_response)
+    
+    def on_prev(self, btn):
+        self.request_image('get_prev')
+    
+    def on_next(self, btn):
+        self.request_image('get_next')
+    
+    def on_rate(self, btn):
+        if self.current_image_id is None or self.current_rating == 0:
+            return
+        data = {'image':self.current_image_id,'rating':self.current_rating}
+        send_request('rate',data, self.on_rate_response)
+    
+    def on_rate_response(self, response):
+        return
+    
+    def on_image_response(self, response):
+        if response.get('status') == 'ok':
+            b64_data = response.get('image')
+            image_bytes = base64.b64decode(b64_data)
+            self.current_image_id = response.get('id')
+            self.current_image_path = response.get('path')
+            ext = 'png'
+            if self.current_image_path:
+                ext = os.path.splitext(self.current_image_path)[1][1:].lower()
+            data_stream = io.BytesIO(image_bytes)
+            core_image = CoreImage(data_stream, ext=ext)
+            self.img.texture = core_image.texture
+            
+            current_rating = response.get('current_rating', 0)
+
+            for i, star in enumerate(self.stars):
+                if i < current_rating:
+                    star.background_normal = 'beginner_git/star1.png'
+                    star.background_down = 'beginner_git/star1.png'
+                else:
+                    star.background_normal = 'beginner_git/star0.png'
+                    star.background_down = 'beginner_git/star0.png'
+
+
+
+
 
 LikeApp().run()
--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import os
 import socket
 import threading
 import json
+import base64
 
 from db import MiniDB
 
@@ -15,6 +16,10 @@ RATINGS_DB = 'ratings.txt'
 
 CLEAN_ON_START = False
 
+if CLEAN_ON_START:
+    open(IMAGES_DB, 'w').close()
+    open(RATINGS_DB, 'w').close()
+
 images = MiniDB(['id', 'path'], IMAGES_DB)
 ratings = MiniDB(['ip','image', 'rating'], RATINGS_DB)
 
@@ -24,12 +29,11 @@ def update_user_position(user, shift, default_value):
     total = len(images.data)
     current = user_position.get(user, default_value)
     new_index = (current + shift) %total
+    user_position[user] = new_index
     return new_index
 
 
-if CLEAN_ON_START:
-    open(IMAGES_DB, 'w').close()
-    open(RATINGS_DB, 'w').close()
+
 
 def scan_images(path):
     result = []
@@ -60,7 +64,27 @@ def command(action_name):
     return decorator
 
 def get_image_response(user, record, action):
-    pass
+    image_path = record['path']
+    try:
+        with open(image_path, 'rb') as img_file:
+            image_bytes = img_file.read()
+    except Exception as e:
+        return {'status':'error', 'error':f'{e}'}
+    b64_data = base64.b64encode(image_bytes).decode('utf-8')
+    current_rating = 0
+    for r in ratings.data:
+        if r["ip"] == user and r['image'] == record['id']:
+            current_rating = int(r['rating'])
+    
+    response = {
+        'status':'ok',
+        'action':action,
+        'image':b64_data,
+        'id':record['id'],
+        'path':image_path,
+        'current_rating':current_rating
+    }
+    return response
 
 @command("get_next")
 def cmd_next(user, request):
@@ -76,7 +100,23 @@ def cmd_prev(user, request):
 
 @command("rate")
 def cmd_rate(user, request):
-    pass
+    image_id = request.get('image')
+    ratings_value = request.get('rating')
+
+    if image_id is None or ratings_value is None:
+        return {'status':'error', 'error':'id or rating is None'}
+    
+    rating_found = False
+    for r in ratings.data:
+        if r['ip'] == user and r['image'] == image_id:
+            r['rating'] = ratings_value
+            rating_found = True
+            break
+    
+    if not rating_found:
+        ratings.add((user, image_id, ratings_value))
+    ratings.save_to_file(RATINGS_DB)
+    return {'status':'ok', 'action':'rate'}
 
 # @command("useless")
 # def useless():
@@ -87,13 +127,16 @@ def cmd_rate(user, request):
 def handle_client(client_socket, addr):
     user_id = addr[0]
     try:
-        while True:
-            data = client_socket.recv(1024).decode()
-            if not data:
-                break
-            request = json.loads(data)
-            action = request.get("action") #request["action"]
-            #commands?
+        data = client_socket.recv(1024).decode()
+        if not data:
+            return
+        request = json.loads(data)
+        action = request.get("action") #request["action"]
+        if action in COMMANDS:
+            response = COMMANDS[action](user_id, request)
+        else:
+            response = None
+        client_socket.send(json.dumps(response).encode())
     except Exception as e:
         print(f"error {e}")
     finally:
@@ -109,7 +152,7 @@ def start_server():
     while True:
         client_socket, addr = server.accept()
         print(f"connected client: {addr}")
-        #thread
+        threading.Thread(target=handle_client, args=(client_socket, addr), daemon=True).start()
 
 if __name__=='__main__':
     start_server()
